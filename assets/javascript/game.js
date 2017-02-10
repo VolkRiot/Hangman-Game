dfe54541234c238e5f001a5e8c6bd048e27373e0
Mikhail Metrikin <metrikin@gmail.com>
2017-02-10 15:17:47 -0800

    Installing win/loss counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,92 +2,106 @@
 
 var Hangman = {
 
-    films: ["It's a Wonderful Life", "Raider's of the Lost Arc", "Star Wars", "Star Trek",
-        "Full Metal Jacket", "Clockwork Orange", "Titanic", "The Godfather", "Forrest Gump",
-        "Taxi Driver", "Avatar", "2001: A Space Odyssey", "The Lion King", "Back to the Future",
-        "Rocky", "Pulp Fiction", "The Matrix", "Jaws", "Jurassic Park", "Airplane!", "Braveheart"],
+  films: ["It's a Wonderful Life", "Raider's of the Lost Arc", "Star Wars", "Citizen Kane",
+    "Full Metal Jacket", "Clockwork Orange", "Titanic", "The Godfather", "Forrest Gump",
+    "Taxi Driver", "Avatar", "2001: A Space Odyssey", "The Lion King", "Back to the Future",
+    "Rocky", "Pulp Fiction", "The Matrix", "Jaws", "Jurassic Park", "Airplane!", "Braveheart"],
 
-    wordChoice: function(){return this.films[Math.floor(Math.random() * this.films.length)]},
-    displayArray: [],
-    wrongGuesses: [],
-    prepWord: function (word) {
-
-        for (var i = 0; i < word.length; i++) {
-            if(/^[a-zA-Z\s]*$/.test(word.charAt(i))) {
-                if (word.charAt(i) == ' ') {
-                    this.displayArray.push('&nbsp;');
-                } else {
-                    this.displayArray.push('_');
-                }
-            }else{
-                this.displayArray.push(word.charAt(i));
-            }
+  wordChoice: function () {
+    return this.films[Math.floor(Math.random() * this.films.length)]
+  },
+  displayArray: [],
+  wrongGuesses: [],
+  totalLetters: 0,
+  prepWord: function (word) {
+    this.totalLetters = 0;
+    for (var i = 0; i < word.length; i++) {
+      if (/^[a-zA-Z\s]*$/.test(word.charAt(i))) {
+        if (word.charAt(i) == ' ') {
+          this.displayArray.push('&nbsp;');
+        } else {
+          this.displayArray.push('_');
+          this.totalLetters++;
         }
-    },
-    /*reset: function () {
-        this.wordChoice();
-        this.displayArray = [];
-        this.wrongGuesses = [];
-    },*/
-    printArray: function (targetTag, array) {
-        $(targetTag).html(array.join(" "));
+      } else {
+        this.displayArray.push(word.charAt(i));
+      }
     }
+  },
+  /*reset: function () {
+   this.displayArray = [];
+   this.wrongGuesses = [];
+   this.totalLetter = 0;
+   },*/
+  printArray: function (targetTag, array) {
+    $(targetTag).html(array.join(" "));
+  }
 };
 
 // Begins actual document run
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var chosenWord;
-    var correctGuesses = 0;
+  var chosenWord;
+  var correctGuesses = 0;
+  var wins = 0;
+  var losses = 0;
 
-    $("#play").on('click', function(){
+  $("#play").on('click', function () {
 
-        chosenWord = Hangman.wordChoice();
+    chosenWord = Hangman.wordChoice();
+    correctGuesses = 0;
+    Hangman.displayArray = [];
+    Hangman.wrongGuesses = [];
+
+    Hangman.prepWord(chosenWord);
+    Hangman.printArray("#tried-array", Hangman.wrongGuesses);
+    Hangman.printArray("#word-array", Hangman.displayArray);
+  });
+
+  $(document).on('keyup', function () {
+
+    var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+    var letterIndex = chosenWord.toLowerCase().indexOf(userInput);
+    var updated = false;
+
+
+    if ((letterIndex < 0) && (Hangman.wrongGuesses.indexOf(userInput.toUpperCase()) == -1)) {
+      Hangman.wrongGuesses.push(userInput.toUpperCase());
+      Hangman.printArray("#tried-array", Hangman.wrongGuesses);
+
+      if (Hangman.wrongGuesses.length > 5) {
+
+        // Ends condition code goes here
+        losses++;
+        $("#loss-counter").html(losses);
+        //Hangman.reset();
+
+      }
+
+
+    } else {
+      for (var i = 0; i < chosenWord.length; i++) {
+        if ((chosenWord[i].toLowerCase() == userInput.toLowerCase()) && (Hangman.displayArray[i] != chosenWord[i])) {
+          Hangman.displayArray[i] = chosenWord[i];
+          correctGuesses++;
+          console.log("Correct Guesses " + correctGuesses);
+          updated = true;
+        }
+      }
+      if (correctGuesses == Hangman.totalLetters) {
+
+        // Win Condition Code goes here.
+        console.log("Total Letters is " + Hangman.totalLetters);
+        wins++;
+        $("#win-counter").html(wins);
         correctGuesses = 0;
-        Hangman.displayArray = [];
-        Hangman.wrongGuesses = [];
+        //Hangman.reset()
+      }
 
-        Hangman.prepWord(chosenWord);
-        Hangman.printArray("#tried-array", Hangman.wrongGuesses);
-        Hangman.printArray("#word-array", Hangman.displayArray);
-    });
-
-    $(document).on('keyup', function (){
-
-        var userInput = String.fromCharCode(event.keyCode).toLowerCase();
-        var letterIndex = chosenWord.toLowerCase().indexOf(userInput);
-        var updated = false;
-
-
-        if ((letterIndex < 0) && (Hangman.wrongGuesses.indexOf(userInput.toUpperCase()) == -1)) {
-            Hangman.wrongGuesses.push(userInput.toUpperCase());
-            Hangman.printArray("#tried-array", Hangman.wrongGuesses);
-
-            if(Hangman.wrongGuesses.length > 5){
-
-                // Ends condition code goes here
-                //Hangman.reset();
-            }
-
-
-        }else{
-            for(var i = 0; i < chosenWord.length; i++){
-                if(chosenWord[i].toLowerCase() == userInput.toLowerCase()){
-                    Hangman.displayArray[i] = chosenWord[i];
-                    correctGuesses++;
-                    updated = true;
-                }
-            }
-            if(correctGuesses == chosenWord.length){
-
-                // Win Condition Code goes here.
-                //Hangman.reset();
-            }
-
-        }
-        if(updated === true){
-            Hangman.printArray("#word-array", Hangman.displayArray);
-        }
-    });
+    }
+    if (updated === true) {
+      Hangman.printArray("#word-array", Hangman.displayArray);
+    }
+  });
 });
