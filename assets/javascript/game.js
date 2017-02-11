0e39530355fdc97f0ba4e896552fdf4fca6d6b4c
Mikhail Metrikin <metrikin@gmail.com>
2017-02-10 23:28:28 -0800

    Rebuild data into objarray with posters-img

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,13 +7,103 @@ var losses = 0;
 
 var Hangman = {
 
-  films: ["It's a Wonderful Life", "Raider's of the Lost Arc", "Star Wars", "Citizen Kane",
-    "Full Metal Jacket", "Clockwork Orange", "Titanic", "The Godfather", "Forrest Gump",
-    "Taxi Driver", "Avatar", "2001: A Space Odyssey", "The Lion King", "Back to the Future",
-    "Rocky", "Pulp Fiction", "The Matrix", "Jaws", "Jurassic Park", "Airplane!", "Braveheart", "Kill Bill"],
+  films: [{
+    "title": "It's a Wonderful Life",
+    "imgPath": "assets/images/moviePosters/itsawonderfullife.jpeg"
+    },
+    {
+      "title": "Raider's of the Lost Arc",
+      "imgPath": "assets/images/moviePosters/raidersofthelostarc.jpeg"
+    },
+    {
+      "title": "Star Wars",
+      "imgPath": "assets/images/moviePosters/starwars.jpg"
+    },
+    {
+      "title": "Citizen Kane",
+      "imgPath": "assets/images/moviePosters/citizenkane.jpeg"
+    },
+    {
+      "title": "Full Metal Jacket",
+      "imgPath": "assets/images/moviePosters/fullmetaljacket.jpeg"
+    },
+    {
+      "title": "Clockwork Orange",
+      "imgPath": "assets/images/moviePosters/clockworkorange.jpg"
+    },
+    {
+      "title": "Titanic",
+      "imgPath": "assets/images/moviePosters/titanic.jpeg"
+    },
+    {
+      "title": "The Godfather",
+      "imgPath": "assets/images/moviePosters/thegodfather.jpg"
+    },
+    {
+      "title": "Forrest Gump" ,
+      "imgPath": "assets/images/moviePosters/forrestgump.jpeg"
+    },
+    {
+      "title": "Taxi Driver",
+      "imgPath": "assets/images/moviePosters/taxidriver.jpeg"
+    },
+    {
+      "title": "Avatar",
+      "imgPath": "assets/images/moviePosters/avatar.jpeg"
+    },
+    {
+      "title": "2001: A Space Odyssey",
+      "imgPath": "assets/images/moviePosters/2001spaceoddysey.jpeg"
+    },
+    {
+      "title": "The Lion King",
+      "imgPath": "assets/images/moviePosters/lionking.jpeg"
+    },
+    {
+      "title": "Back to the Future",
+      "imgPath": "assets/images/moviePosters/backtothefuture.jpeg"
+    },
+    {
+      "title": "Rocky",
+      "imgPath": "assets/images/moviePosters/rocky.jpeg"
+    },
+    {
+      "title": "Pulp Fiction",
+      "imgPath": "assets/images/moviePosters/pulpfiction.jpg"
+    },
+    {
+      "title": "The Matrix",
+      "imgPath": "assets/images/moviePosters/thematrix.jpeg"
+    },
+    {
+      "title": "Jaws",
+      "imgPath": "assets/images/moviePosters/jaws.jpg"
+    },
+    {
+      "title": "Jurassic Park",
+      "imgPath": "assets/images/moviePosters/jurassicpark.jpeg"
+    },
+    {
+      "title": "Airplane!",
+      "imgPath": "assets/images/moviePosters/airplane.jpeg"
+    },
+    {
+      "title": "Braveheart",
+      "imgPath": "assets/images/moviePosters/braveheart.jpg"
+    },
+    {
+      "title": "Kill Bill",
+      "imgPath": "assets/images/moviePosters/killbill.jpeg"
+    },
+    {
+      "title": "E.T. the Extra-Terrestrial",
+      "imgPath": "assets/images/moviePosters/ettheextraterrestrial.jpeg"
+    }
+  ],
+
   stickFig:["assets/images/hangman-imgs/Hangman-0.png", "assets/images/hangman-imgs/Hangman-1.png", "assets/images/hangman-imgs/Hangman-2.png", "assets/images/hangman-imgs/Hangman-3.png", "assets/images/hangman-imgs/Hangman-4.png", "assets/images/hangman-imgs/Hangman-5.png", "assets/images/hangman-imgs/Hangman-6.png"],
   wordChoice: function () {
-    return this.films[Math.floor(Math.random() * this.films.length)]
+    return this.films[Math.floor(Math.random() * this.films.length)]["title"];
   },
   displayArray: [],
   wrongGuesses: [],
@@ -56,6 +146,15 @@ function newWord() {
   $("#hangman-image").attr("src", Hangman.stickFig[0]);
 }
 
+function indexByValue(objArray, key, value) {
+  for(var i = 0; i < objArray.length; i++){
+    if(objArray[i][key] == value){
+      return i;
+    }
+  }
+  return -1;
+}
+
 $(document).ready(function () {
 
     // Reset whole game
@@ -76,7 +175,7 @@ $(document).ready(function () {
 
       if (Hangman.wrongGuesses.length > 5) {
 
-        // Game over condition code goes here
+        // Loss logic starts here
 
         losses++;
         $("#loss-counter").html(losses);
@@ -84,7 +183,7 @@ $(document).ready(function () {
         resetAll();
         newWord();
 
-        // Needs to stop game show or loss artifact
+        // TODO: Possible losing condition feedback
 
       }
 
@@ -99,21 +198,22 @@ $(document).ready(function () {
       }
       if (correctGuesses == Hangman.totalLetters) {
 
-        // Win Condition Code goes here.
+        // Win Condition Code goes below.
 
         wins++;
         $("#win-counter").html(wins);
+        var objIndex = indexByValue(Hangman.films, "title" ,chosenWord);
+        $("#poster-img").attr("src", Hangman.films[objIndex]["imgPath"]);
+        $("#poster-img").attr("alt", Hangman.films[objIndex]["title"]);
         correctGuesses = 0;
 
-        // Remove winning word from the array to avoid repetition
-        Hangman.films.splice(Hangman.films.indexOf(chosenWord), 1);
+        // TODO: Possibly remove winning title from obj array
 
-        //Add code here to reward the player i.e. show graphic etc
-
+        // Reset game (not win/lose record)
         resetAll();
         newWord();
 
-        //Needs to display win and restart game.
+        // TODO: Display winning title, maybe multimedia
 
       }
 
